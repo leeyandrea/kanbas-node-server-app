b3e9de4fa327d20e3bea45c861d9b0b73b56b91c
Andrea <andreayoung5350@gmail.com>
2024-12-05 23:49:57 -0500

    modify netlify url

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "https://kanbas-react-web-app-cs5610-fa24-alee.netlify.app/",
+    origin: [process.env.NETLIFY_URL || "https://kanbas-react-web-app-cs5610-fa24-alee.netlify.app/", "https://67527de--kanbas-react-web-app-cs5610-fa24-alee.netlify.app",
+        /\.netlify\.app$/]
 }));
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kanbas",
