b6092a3f2164b716dfbd0f75ebb32c1f8980a337
Andrea <andreayoung5350@gmail.com>
2024-12-06 13:54:20 -0500

    .env file changed node server domain

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors({
     credentials: true,
-    origin: [process.env.NETLIFY_URL || "https://kanbas-react-web-app-cs5610-fa24-alee.netlify.app/", "https://67527de--kanbas-react-web-app-cs5610-fa24-alee.netlify.app",
+    origin: [process.env.NETLIFY_URL || "http://localhost:3000",
         /\.netlify\.app$/]
 }));
 const sessionOptions = {
